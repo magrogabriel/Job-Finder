2958c7602bdb03edf7065d239abe5aa3bc270026
Gabriel Magro <gabrielmagro.94@gmail.com>
2022-12-02 10:37:43 -0300

    1-commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+const db = require('./db/connection')
+const bodyParser = require('body-parser');
+const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`O Express está rodando na porta ${PORT}.`);
+});
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// handlebars
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// staticfolder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//db connection (uma promisse)
+db.authenticate()
+.then(() => {
+    console.log("Conectou ao banco com sucesso");
+})
+.catch( err => {
+    console.log("Ocorreu um erro ao conectar", err);
+})
+
+//routes
+app.get('/', (req, res) => {
+
+    let search = req.query.job;
+    let query = '%'+search+'%'; // PH -> PHP, WORD => WORDPRESS
+
+    if(!search) {
+
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+    
+            res.render('index', {
+                jobs
+            });
+        })
+        .catch(err => console.log(err));
+    } else {
+
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+    
+            res.render('index', {
+                jobs, search
+            });
+        })
+
+    }
+
+
+});
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
